0c77f13a4919c43de1e570917ccc6f94148980b2
RhugwedaShedge <rhugwedanshedge@gmail.com>
2021-03-27 20:22:33 +0530

    commit 2 rh

--- ProjectKrishi/farmers/urls.py
+++ ProjectKrishi/farmers/urls.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from django.urls import path
 
-from farmers.views import home_view
+from .views import ( home_view,
+                        about_view,
+                        cart_view,
+                        checkout_view,
+                        contact_us_view,
+                        gallery_view,
+                        my_account_view,
+                        shop_detail_view,
+                        shop_view,
+                        wishlist_view  )
 
 app_name = 'farmers'
 
 urlpatterns = [
 
-    path('home/', home_view, name = 'home'),
+    path('home/', home_view, name = "home"),
+    path('about/', about_view, name = "about"),
+    path('cart/', cart_view, name = "cart"),
+
 ]
+
+
--- ProjectKrishi/farmers/views.py
+++ ProjectKrishi/farmers/views.py
@@ -2,11 +2,47 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-
 def home_view(request, *args, **kwargs):
-	#print(request)
-	#print(request.user)
-	
 
 	return render(request, "farmers/index.html", {})
 
+
+def about_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/about.html", {})
+
+def cart_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/cart.html", {})
+
+def checkout_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/checkout.html", {})
+
+def contact_us_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/contact-us.html", {})
+
+def gallery_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/gallery.html", {})
+
+def checkout_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/checkout.html", {})
+
+def my_account_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/my-account.html", {})
+
+def shop_detail_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/shop-detail.html", {})
+
+def shop_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/shop.html", {})
+
+def wishlist_view(request, *args, **kwargs):
+	
+	return render(request, "farmers/wishlist.html", {})
