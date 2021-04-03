51eddd2bca5e7157c1aa3b4ce12c913226c59c52
RhugwedaShedge <rhugwedanshedge@gmail.com>
2021-04-03 15:35:50 +0530

    commit 10

--- ProjectKrishi/farmers/models.py
+++ ProjectKrishi/farmers/models.py
@@ -21,5 +21,20 @@ class Description(models.Model):
     stock        = models.DecimalField(decimal_places = 0, max_digits = 100)
     desc         = models.CharField(max_length = 500, null = True)
 
+class Product(models.Model):
+	name    = models.CharField(max_length=200)
+	price   = models.FloatField()
+	digital = models.BooleanField(default=False,null=True, blank=True)
+	image   = models.ImageField(null=True, blank=True)
 
+	def __str__(self):
+		return self.name
+
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
