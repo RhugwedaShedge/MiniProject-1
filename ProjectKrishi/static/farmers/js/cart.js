587623edd81ae6f03037afbfdab42c4b2c93eb94
RhugwedaShedge <rhugwedanshedge@gmail.com>
2021-05-29 17:18:13 +0530

    commit 27

--- ProjectKrishi/static/farmers/js/cart.js
+++ ProjectKrishi/static/farmers/js/cart.js
@@ -1,9 +1,11 @@
 var updateBtns = document.getElementsByClassName('update-cart')
-
+console.log(updateBtns)
+console.log("Hiiiiiiiiiiiiiiiiiiiii")
 for (i = 0; i < updateBtns.length; i++)
 {
+    // On each click this function will run
     updateBtns[i].addEventListener('click', function(){
-        var productId = this.dataset.product
+        var productId = this.dataset.product // this is same as self in python
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
     })
